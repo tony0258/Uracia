014094262451f5e8130f03804ab378c9bd7051f4
Uracia <kimminnu85@gmail.com>
2024-05-06 22:36:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include °∞myProject.h°±
+int main(void)
+{
+	printf(°∞Chief name is[∆¿¿Â¿Ã∏ß]\n°±);
+	printMyName1();
+	printMyName2();
+	printMyName3();
+	printMyName4();
+	
+}
 
