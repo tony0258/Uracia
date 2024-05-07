9bbf422dd344d36c9ac5680da20aacbd2c0ed977
tony0258 <130204182+tony0258@users.noreply.github.com>
2024-05-07 20:08:48 +0900

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "myProject.h"
+int main(void)
+{
+        print ("Chief name is [팀장이름]\n");
+        printMyName1();
+        printMyName2();
+        printMyName3();
+        printMyName4("정현준");
+}
