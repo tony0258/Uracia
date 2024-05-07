0eab98b702ee4b32a08a941762973a81349cba4a
Uracia <kimminnu85@gmail.com>
2024-05-07 14:00:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include ¡°myProject.h¡±
+#include "myProject.h"
 int main(void)
 {
-	printf(¡°Chief name is[Uracia]\n¡±);
+	printf("Chief name is[Uracia]\n");
 	printMyName1();
 	printMyName2();
 	printMyName3();
